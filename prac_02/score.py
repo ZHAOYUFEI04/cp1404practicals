8c97e00ae0f47df8b05e1b255482171d45679e79
ZHAOYUFEI04 <yufei.zhao1@my.jcu.edu.au>
2023-11-19 21:55:33 +0800

    Function for score and print the result and generate a random score.

--- prac_02/score.py
+++ prac_02/score.py
@@ -12,14 +12,28 @@
 # if score < 50:
 #     print("Bad")
 
-score = float(input("Enter score: "))
-if 0 <= score <= 100:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    else:
-        print("Bad")
-else:
-    print("Invalid score")
 
+import random
+
+def determine_result(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score > 0:
+        return "Bad"
+    else:
+        return "Invalid score"
+
+def main():
+    user_score = float(input("Enter score: "))
+    result = determine_result(user_score)
+    print(f"Your result:{result}")
+
+    # Generate ranndom score
+    random_score = random.randint(0,101)
+    print(f"Random score:{random_score}")
+    random_result = determine_result(random_score)
+    print(f"Random result:{random_result}")
+
+main()

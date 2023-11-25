685c9f3eb27f341d3cd051d5c2bedb88a20043df
ZHAOYUFEI04 <yufei.zhao1@my.jcu.edu.au>
2023-11-25 17:16:41 +0800

    Write code, not a comment, to produce a random number between 1 and 100 inclusive

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -3,3 +3,6 @@ import random
 print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive
+print(random.randint(1, 100))

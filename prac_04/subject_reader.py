ec4c7d4414bf4adca5e4f3daf57c04b008cf6d5b
ZHAOYUFEI04 <yufei.zhao1@my.jcu.edu.au>
2023-12-01 11:25:41 +0800

    # WHY can't return the subjects value? I don't know where went wrong

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -3,27 +3,31 @@ CP1404/CP5632 Practical
 Data file -> lists program
 """
 
-FILENAME = "subject_data.txt"
+FILENAME = "subjcet_data.txt"
 
 
 def main():
     data = get_data()
-    print(data)
-
+    display_subject_details(data)
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+    subjects = []
+    with open(FILENAME) as input_file:
+        for line in input_file:
+            line = line.strip()  # Remove the \n
+            parts = line.split(',')  # Separate the data into its parts
+            parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+            subjects.append(parts)
+        # print(subjects)
+        return subjects  # WHY can't return the subjects value? I don't know where went wrong
 
+
+def display_subject_details(data):
+    for subject in data:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students.")
 
 main()
+
+
+

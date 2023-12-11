36ba43e71331759ed34d281e5dfa068b611232cd
ZHAOYUFEI04 <yufei.zhao1@my.jcu.edu.au>
2023-12-11 22:47:22 +0800

    First try prac 7  Project Management Program exercise

---
+++ prac_07/project.py
@@ -0,0 +1,23 @@
+import datetime
+
+class Project:
+    def __init__(self, name, start_date, priority, cost_estimate, percent_complete):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.percent_complete = percent_complete
+
+    def __str__(self):
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, " \
+               f"estimate: ${self.cost_estimate:.2f}, completion: {self.percent_complete}%"
+
+    def update_project(self, percent_complete="", priority=""):
+        if percent_complete:
+            self.percent_complete = percent_complete
+        if priority:
+            self.priority = priority
+
+    def is_after_date(self, date):
+        project_date = datetime.datetime.strptime(self.start_date, "%d/%m/%Y").date()
+        return project_date > date

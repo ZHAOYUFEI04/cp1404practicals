6af750d47aa98b48bd6bc29ca7549c8b3c9d1737
ZHAOYUFEI04 <yufei.zhao1@my.jcu.edu.au>
2023-12-20 14:55:49 +0800

    Complete Prac 8 squaring exercise.

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,7 +1,7 @@
 """
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
+Zhao Yufei, IT@JCU
 Started 13/10/2015
 """
 
@@ -9,7 +9,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Zhao Yufei'
 
 
 class SquareNumberApp(App):
@@ -17,7 +17,7 @@ class SquareNumberApp(App):
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (200, 100)
-        self.title = "Square Number"
+        self.title = "Square Number 2"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
